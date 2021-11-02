e8464265c1d39fd5ff02d84e41d6387eed25a9ed
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-02 20:08:33 +0100

    Add files via upload

--- Ejercicios de Programación/tema 5/ejercicio25.java
+++ Ejercicios de Programación/tema 5/ejercicio25.java
@@ -1,35 +1,19 @@
 /* Realiza un programa que pida un número por teclado y que luego muestre ese
-número al revés. */
+número al revés.*/
 
-public class ejercicio25{
-	public static void main(String[] args){
-/*
-		System.out.print("Introduzca un número entero: ");
-		int numeroIntroducido = Integer.parseInt(System.console().readLine());
-		
-		int num = numeroIntroducido;
-		int volt = 0;
-		
-		while (num > 0) {
-			volt = (volt * 10) + (num % 10);
-			num /= 10;
-		} 
-		System.out.println("Si le damos la vuelta al " + numeroIntroducido + " tenemos el " + volt);
-	}
-}
-*/
-
-
-
-	System.out.print("Introduzca un número entero: ");
+public class ejercicio25 {
+	public static void main(String[] args) {
+	
+		System.out.println("Introduzca un numero : ");
 		int num = Integer.parseInt(System.console().readLine());
+	
 		
-		int volt = 0;
+		int pot = 0;
 		
-		while (num > 0) {
-			volt = (volt * 10) + (num % 10);
-			num /= 10;
-		} 
-		System.out.println("El resultado es: " + volt);
+		while (num > 0){
+		pot = (pot * 10) + (num % 10);
+		num /= 10;		
+		}
+		System.out.println("El resultado es: " + pot );
 	}
 }
