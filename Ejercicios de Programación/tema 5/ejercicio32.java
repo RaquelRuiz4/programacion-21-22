5057b9f6f938bde4d51abe3ab60b23e7cc40ceaf
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-07 23:56:32 +0100

    Add files via upload

--- Ejercicios de Programación/tema 5/ejercicio32.java
+++ Ejercicios de Programación/tema 5/ejercicio32.java
@@ -1,4 +1,4 @@
-/* Escribe un programa que, dado un número entero positivo, diga cuáles son y
+/* eEscribe un programa que, dado un número entero positivo, diga cuáles son y
 cuánto suman los dígitos pares. Los dígitos pares se deben mostrar en orden, de
 izquierda a derecha. Usa long en lugar de int donde sea necesario para admitir
 números largos. */
