e90c4e34337895abe7cd1523bea5e8f038a8f272
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-11 11:14:34 +0100

    Add files via upload

---
+++ Ejercicios de Programación/tema 6/ejercicio9.java
@@ -0,0 +1,22 @@
+/* Realiza un programa que vaya generando números aleatorios pares entre 0
+y 100 y que no termine de generar números hasta que no saque el 24. El
+programa deberá decir al final cuántos números se han generado. */
+
+public class ejercicio9{
+	public static void main(String[] args){
+		int num;
+		int cont = 0;
+	
+	do{
+		num = (int)(Math.random()*101);
+		System.out.println(num);
+		cont++;
+	}
+	
+	while(num != 24);
+	System.out.println("Hay un total de " + cont + " numeros antes de que haya salido el 24.");
+	
+	
+	
+	}
+}
