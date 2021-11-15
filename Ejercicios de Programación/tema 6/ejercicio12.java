9c4ec5b33312cd176815d014e894fe30b041cc84
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-15 11:15:15 +0100

    Add files via upload

--- Ejercicios de Programación/tema 6/ejercicio12.java
+++ Ejercicios de Programación/tema 6/ejercicio12.java
@@ -4,51 +4,9 @@ convertir un entero en un carácter. */
 
 public class ejercicio12{
 	public static void main(String[] args){
-	
-		int numNotas = 0;
-		String notas = "";
 		
-		for (int i = 1; i <= 7; i++){
-			numNotas = (int)(Math.random()*7)+1;
-		
-		switch (numNotas){
-			case 1:
-					notas = "do";
-					
-				break;
-				case 2: 
-					notas = "re";
-
-				break;
-				case 3:
-					notas = "mi";
-				break;
-				case 4:
-					notas = "fa";
-				break;
-				case 5:
-					notas = "sol";
-				break;
-				case 6:
-					notas = "la";
-				break;
-				case 7:
-					notas = "si";
-				break;
-				default:
-			}
-			
-			System.out.print(notas + " ");
-			
-			
-			
+		for (int i = 1; i <= 5000; i++){
+			System.out.print((char)(Math.random() * (126 - 32 + 1) + 32));
 		}
-	
-	
-	
-	
-	
-	
-	
 	}
 }
