a3cdc2f83c3862013824ab8396c7f25646a0d5f8
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-16 22:53:42 +0100

    Add files via upload

--- Ejercicios de Programación/tema 6/ejercicio6.java
+++ Ejercicios de Programación/tema 6/ejercicio6.java
@@ -1,34 +1,31 @@
-/* Escribe un programa que piense un número al azar entre 0 y 100. El usuario debe adivinarlo y tiene para ello 5 oportunidades. Después de cada intento fallido,
-el programa dirá cuántas oportunidades quedan y si el número introducido es
-menor o mayor que el número secreto */
+/* Escribe un programa que lea 15 números por teclado y que los almacene en un
+array. Rota los elementos de ese array, es decir, el elemento de la posición 0
+debe pasar a la posición 1, el de la 1 a la 2, etc. El número que se encuentra en
+la última posición debe pasar a la posición 0. Finalmente, muestra el contenido
+del array. */
 
 public class ejercicio6{
 	public static void main(String[] args){
-	
-	boolean acierto = false;
-	
 		
-		int num = (int)(Math.random()*101);
-		System.out.print(num);
-		System.out.println();
-		int intento = 0;
-		int cont = 5;
-		do {
-			
-			System.out.println("Tienes " + cont + " intentos.");
-			intento = Integer.parseInt(System.console().readLine());
-			if(num != intento){
-				if (intento < num){
-					System.out.println("El numero que has introducido es menor.");
-				} else {
-					System.out.println("El numero que has introducido es mayor.");
-				}
-			} else {
-				acierto = true;
-				System.out.println("Has acertado.");
-			}
-			cont--;
+		int[] array = new int[15];
+		
+		for(int i = 0; i < 15; i++){
+			array[i] = Integer.parseInt(System.console().readLine());
+		}	
+		
+		for(int i = 0; i < 15; i++){
+			System.out.print(array[i] + " ");
 		}
-		while (cont > 0 && !acierto);	
+		
+		int aux = array[14];
+		
+		for(int i = 14; i > 0; i--){
+			array[i] = array[i-1];
+		}
+		array[0] = aux;
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.print(i);
+		}
 	}
 }
