54ae08b690573336dd32caa28ca8392ba1c34555
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-18 00:04:42 +0100

    Add files via upload

---
+++ Ejercicios de Programación/tema 7/ejercicio11.java
@@ -0,0 +1,63 @@
+/* Realiza un programa que pida 10 números por teclado y que los almacene en
+un array. A continuación se mostrará el contenido de ese array junto al índice
+(0 – 9) utilizando para ello una tabla. Seguidamente el programa pasará los
+primos a las primeras posiciones, desplazando el resto de números (los que no
+son primos) de tal forma que no se pierda ninguno. Al final se debe mostrar el
+array resultante. */
+
+
+public class ejercicio11{
+	public static void main(String[] args){
+		
+		int[] array = new int[20];
+		int[] pares = new int[20];
+		int[] impares = new int[20];
+		int par = 0;
+		int impar = 0;
+		int i = 0;
+		
+		System.out.println("Introduzca 10 numeros.");
+		for (i = 0; i <= 9; i++){
+			array[i] = Integer.parseInt(System.console().readLine());
+			
+		}
+		System.out.print("Indice ");
+		for (i = 0; i <= 9; i++){
+			System.out.print(i + "    ");
+		}
+		System.out.println();
+		System.out.print("Valor ");
+		for (i = 0; i <= 9; i++){
+			System.out.print(" " + array[i] + "    ");
+		}
+		for (i = 2; i <= 9; i++){
+			if (array[i] ){
+					
+				}
+		}
+		
+	
+		/*for (int i = 0; i <= 19; i++){
+			if (array[i] % 2 == 0){
+				 pares[par++] = array[i];
+				
+			} else {
+				impares[impar++] = array[i];		
+			}
+		}		
+		System.out.println();
+		System.out.println();
+		System.out.println("Array primero pares y despues impares.");
+		for (int i = 0; i <= par; i++){
+			array[i] = pares[i];
+			
+		}
+		for (int i = par; i < 20; i++){	
+			array[i] = impares[i - par];
+		}
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.print(array[i] + " ");
+		} */
+	}
+}
