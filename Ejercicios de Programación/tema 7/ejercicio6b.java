3265644ae41d75762a8174af86f5a0f779699e75
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-11-29 22:42:53 +0100

    Add files via upload

--- Ejercicios de Programación/tema 7/ejercicio6b.java
+++ Ejercicios de Programación/tema 7/ejercicio6b.java
@@ -1,20 +1,38 @@
 /* Modifica el programa anterior de tal forma que no se repita ningún número en
-el array. */
+el array. */ 
 
 public class ejercicio6b{
 	public static void main(String[] args){
 		
 		int[][] arr = new int[6][10];
+		int aleatorios;
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
 		
+		
+		
 		for(int i = 0; i < 6; i++){
 			for(int j = 0; j < 10; j++){
-				arr[i][j] = (int)(Math.random()*1001);
+				boolean repe = false;
+				do{
+					aleatorios = (int)(Math.random()*1001);
+					repe = false;
+					for(int m = 0; m <= i; m++){
+						for(int n = 0; n < j || (n < 10 && m!= i); n++){
+							if ( arr[m][n] == aleatorios){
+								repe = true;								
+							}
+						}
+					}
+				} while(repe);
+				arr[i][j] = aleatorios;
 				System.out.printf("%4d", arr[i][j]);
 			}
 			System.out.println();
 		}
+		
+		System.out.println();
+		
 		for(int i = 0; i < 6; i++){
 			for(int j = 0; j < 10; j++){
 				if (arr[i][j] < min){
@@ -25,6 +43,7 @@ public class ejercicio6b{
 				}
 			}
 		}
+		
 		System.out.println("El numero minimo es: " + min);
 		System.out.print("El numero maximo es: " + max);
 	}
