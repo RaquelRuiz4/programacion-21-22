dbda88bf037c8f34afde84627a42fa827743ef9f
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-07 00:09:01 +0100

    Add files via upload

---
+++ Ejercicios de Programación/tema 7/ejercicio12b.java
@@ -0,0 +1,49 @@
+/* Realiza un programa que muestre por pantalla un array de 9 filas por 9
+columnas relleno con números aleatorios entre 500 y 900. A continuación, el
+programa debe mostrar los números de la diagonal que va desde la esquina
+inferior izquierda a la esquina superior derecha, así como el máximo, el mínimo
+y la media de los números que hay en esa diagonal. */
+
+public class ejercicio12b{
+	public static void main(String[] args){
+	
+	int fila = 9;
+	int columna = 9;
+	
+	int[][] array = new int[fila][columna];
+	int max = 900;
+	int min = 500;
+	
+	for(int i = 0; i < fila; i++){
+		for(int j = 0; j < columna; j++){
+		array[i][j] = (int)(Math.random()*(max-min+1)+min);	
+		}
+	}
+	
+	for(int i = 0; i < fila; i++){
+		for(int j = 0; j < columna; j++){
+			System.out.printf("%6d", array[i][j]);
+		}
+		System.out.println();
+	}
+	System.out.println();
+	// recorrer diagonal de array
+	
+	for(int i=array.length-1; i>=0; i--){
+		System.out.printf("%6d", array[i][array.length-1-i]);
+		}
+		System.out.println();
+	for(int i=0; i<=array.length-1; i++){
+		System.out.printf("%6d", array[i][array.length-1-i]);
+		}
+		System.out.println();
+	for(int i=0; i<=array.length-1; i++){
+		System.out.printf("%6d", array[i][i]);
+		}
+		System.out.println();
+	for(int i=array.length-1; i>=0; i--){
+		System.out.printf("%6d", array[i][i]);
+		}
+	
+	}
+}
