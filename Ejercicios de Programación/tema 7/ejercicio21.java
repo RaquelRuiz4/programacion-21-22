ed4e40ecbb79ec9dfcb7012b217f61b28f5ffb92
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-08 22:48:32 +0100

    Add files via upload

---
+++ Ejercicios de Programación/tema 7/ejercicio21.java
@@ -0,0 +1,29 @@
+/* Escribe un programa que rellene un array de 15 elementos con números enteros
+comprendidos entre 0 y 500 (ambos incluidos). A continuación, se mostrará el
+array cincuerizado, según el siguiente criterio: si el número que hay en una
+posición del array es múltiplo de 5, se deja igual, y si no, se cambia por el
+siguiente múltiplo de 5 que exista a partir de él. */
+
+public class ejercicio21{
+	public static void main(String[] args){
+		
+		int[] array = new int[15];
+		
+		int min = 0; 
+		int max = 500;
+		
+		for(int i = 0;  i < 15; i++){
+			array[i] = (int)(Math.random()*(max-min+1)+min);
+		}		
+		
+		for(int i = 0;  i < 15; i++){
+			while(array[i] % 5 != 0){				
+				array[i]++;
+			} 
+		}
+		
+		for(int i = 0;  i < 15; i++){
+			System.out.printf("%4d", array[i]);
+		}
+	}
+}
