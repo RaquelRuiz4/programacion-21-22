c074aab6fc70e8b118df3d4816dedef5d9e2ce39
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-13 23:22:44 +0100

    Add files via upload

--- Ejercicios de Programación/Ejercicios Examenes/ejercicioExamen2a.java
+++ Ejercicios de Programación/Ejercicios Examenes/ejercicioExamen2a.java
@@ -17,65 +17,54 @@ formato: */
 
 public class ejercicioExamen2a{
 	public static void main(String[] args){
-		System.out.print("Indique el tamaño de la matriz bidimensional - filas: ");
+	
+	System.out.print("Introduzca la cantidad de elementos del conjunto aleatorio base: ");
+	int cant = Integer.parseInt(System.console().readLine());
+	
+	int relleno = 0;
+	int cont = 0;
+	
+	int[] numArray = new int[cant];
+	boolean repe = false;
+	for (int j = 0; j < cant; j++){
+		do{
+			repe = false;
+			System.out.print("Introduzca un valor aleatorio base: ");
+			relleno = Integer.parseInt(System.console().readLine());
+			for (int k = 0; k < numArray.length; k++){
+			 if(numArray[k] == relleno){
+				repe = true;
+				System.out.print("Dato repetido. ");
+			} 
+		}
+		} while(repe);
+		numArray[cont++] = relleno;
+	}
+	System.out.println("Los valores que podrán ser elegidos aleatoriamente son los siguientes: ");
+    for (int i = 0; i < numArray.length; i++){
+      System.out.printf("%4d ",numArray[i]);
+    }
+    System.out.println();
+    
+  	System.out.print("Indique el tamaño de la matriz bidimensional - filas: ");
 	int fila = Integer.parseInt(System.console().readLine());
 	System.out.print("Indique el tamaño de la matriz bidimensional - columnas: ");
 	int columna = Integer.parseInt(System.console().readLine());	
 	
 	int[][] bidi = new int[fila][columna];
-	
-	System.out.print("Indique extremo inferior rango aleatorio para rellenar matriz: ");
-	int min = Integer.parseInt(System.console().readLine());
-	System.out.print("Indique extremo superior rango aleatorio para rellenar matriz: ");	
-	int max = Integer.parseInt(System.console().readLine());
-	
-	int extracciones; 
-	
-	 do{
-		System.out.print("Indique la cantidad de valores aleatorios que desea extraer: ");
-		extracciones = Integer.parseInt(System.console().readLine());
-		if(extracciones > fila*columna){
-			System.out.print("Valor excesivo. ");
-		}
-    } 
-    while(extracciones > fila*columna);
+	int numElegidos = 0;
     
-    
-    
-	for(int i = 0; i < fila; i++){
-		for(int j = 0; j < columna; j++){
-			bidi[i][j] = (int)(Math.random()*(max-min+1)+min);
-		}
-	}
-	
-	for(int i = 0; i < fila; i++){
-			for(int j = 0; j < columna; j++){
-				System.out.printf("%4d", bidi[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-		
-		int[] arrayPosiciones = new int[extracciones];
-		boolean repetido = false;
-		int filaRandom;
-		int columnaRandom;
-		int cont = 0;
-		for(int i = 0; i < extracciones; i++){
-			do{		
-				repetido = false;		
-				filaRandom = (int)(Math.random()*fila);
-				columnaRandom = (int)(Math.random()*columna);
-				for(int j = 0; j< extracciones; j++){
-					if (arrayPosiciones[j] == filaRandom*columnaRandom+columna){
-						repetido = true;
-					}
-				}
-			}
-			while(repetido);
-			arrayPosiciones[cont++] = filaRandom*columnaRandom+columna;
-			
-			System.out.printf("%4d", bidi[filaRandom][columnaRandom]);
-		}  
+    for (int i = 0; i <fila ; i++){
+      for (int j = 0; j < columna; j++){
+        numElegidos = (int)(Math.random()*relleno);
+        bidi[i][j]= numArray[numElegidos];
+      }      
+    }    
+    for (int i = 0; i < fila; i++){
+        for (int k = 0; k < columna; k++){
+          System.out.printf("%3d ",bidi[i][k]);
+        }
+        System.out.println();
+        }  
 	}
 }
