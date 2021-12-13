f10703491f9dbc3966e677ad3db9fd9b51f8a3bd
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-14 00:33:50 +0100

    Add files via upload

--- Ejercicios de Programación/Ejercicios Examenes/ejercicioExamen3a.java
+++ Ejercicios de Programación/Ejercicios Examenes/ejercicioExamen3a.java
@@ -1,4 +1,4 @@
-/* ] Dada una frase introducida por teclado, el programa debe sustituir las ocurrencias de una 
+/* Dada una frase introducida por teclado, el programa debe sustituir las ocurrencias de una 
 determinada letra por otra. La primera frase y las letras a sustituir una por otra serán preguntadas por el 
 programa. El programa deberá presentar por pantalla la frase original y la frase con las sustituciones 
 realizadas. Finalmente, se le preguntará al usuario si desea salir. En caso negativo, se tomará como frase 
@@ -9,65 +9,44 @@ Observe el ejemplo; preste atención al formato: */
 
 public class ejercicioExamen3a{
 	public static void main(String[] args){
-		System.out.print("Indique el tamaño de la matriz bidimensional - filas: ");
-	int fila = Integer.parseInt(System.console().readLine());
-	System.out.print("Indique el tamaño de la matriz bidimensional - columnas: ");
-	int columna = Integer.parseInt(System.console().readLine());	
-	
-	int[][] bidi = new int[fila][columna];
-	
-	System.out.print("Indique extremo inferior rango aleatorio para rellenar matriz: ");
-	int min = Integer.parseInt(System.console().readLine());
-	System.out.print("Indique extremo superior rango aleatorio para rellenar matriz: ");	
-	int max = Integer.parseInt(System.console().readLine());
-	
-	int extracciones; 
-	
-	 do{
-		System.out.print("Indique la cantidad de valores aleatorios que desea extraer: ");
-		extracciones = Integer.parseInt(System.console().readLine());
-		if(extracciones > fila*columna){
-			System.out.print("Valor excesivo. ");
-		}
-    } 
-    while(extracciones > fila*columna);
-    
-    
-    
-	for(int i = 0; i < fila; i++){
-		for(int j = 0; j < columna; j++){
-			bidi[i][j] = (int)(Math.random()*(max-min+1)+min);
-		}
-	}
-	
-	for(int i = 0; i < fila; i++){
-			for(int j = 0; j < columna; j++){
-				System.out.printf("%4d", bidi[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
 		
-		int[] arrayPosiciones = new int[extracciones];
-		boolean repetido = false;
-		int filaRandom;
-		int columnaRandom;
-		int cont = 0;
-		for(int i = 0; i < extracciones; i++){
-			do{		
-				repetido = false;		
-				filaRandom = (int)(Math.random()*fila);
-				columnaRandom = (int)(Math.random()*columna);
-				for(int j = 0; j< extracciones; j++){
-					if (arrayPosiciones[j] == filaRandom*columnaRandom+columna){
-						repetido = true;
-					}
-				}
+		String frase = "";
+		String fraseFinal = "";
+		String salir = "";
+		System.out.print("Introduzca una frase:" );
+		frase = System.console().readLine();
+    
+    do {
+		System.out.print("Indique la letra que desea sustituir:" );
+		 String letra = System.console().readLine();
+		
+		System.out.print("Indique la letra sustituta: " );
+		 String letraCambia = System.console().readLine();
+    
+    
+		System.out.print("Frase original: " );
+		System.out.print(frase);
+		
+        System.out.println();
+        
+		System.out.print("Frase alterada: " );
+		fraseFinal = frase.replace(letra, letraCambia);
+		System.out.print(fraseFinal);
+		System.out.println();
+		System.out.print("¿Desea salir? (s/n): ");
+		salir = System.console().readLine();
+		
+	} while (!(salir.equals("s")));
+	
+		
+		/* for(int i = 0; i < frase.length(); i++){
+			if (frase.charAt(i) == letra.){
+				frase.charAt(i) = letraCambia;
 			}
-			while(repetido);
-			arrayPosiciones[cont++] = filaRandom*columnaRandom+columna;
-			
-			System.out.printf("%4d", bidi[filaRandom][columnaRandom]);
-		}  
+	} */ 
+    
+    
+  
+   
 	}
 }
