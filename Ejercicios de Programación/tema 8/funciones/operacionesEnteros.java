6bd3989f367acf21853c19d764306e1f7a6b87fc
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-24 15:04:24 +0100

    Add files via upload

---
+++ Ejercicios de Programación/tema 8/funciones/operacionesEnteros.java
@@ -0,0 +1,46 @@
+package funciones;
+
+public class operacionesEnteros{
+	
+	// esCapicua: Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
+	public static boolean esCapicua(int num){
+		if(num == voltea(num)){
+			return true;
+		}
+		return false;
+	}
+	// voltea: Le da la vuelta a un número.
+	public static int voltea(int num){
+		int vuelta = 0;
+		while(num > 0){
+			vuelta = (vuelta * 10) + num%10;
+			num /= 10;
+		}
+		return vuelta;
+	}	
+	
+	// esPrimo: Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso contrario.
+	public static boolean esPrimo(int num){
+		for(int i = 2; i < num; i++){
+			if (num % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+		
+	// siguientePrimo: Devuelve el menor primo que es mayor al número que se pasa como parámetro.
+	public static int siguientePrimo(int num){
+		do{
+			num++;
+			}
+		while(!esPrimo(num));
+		return num;
+	}
+		
+		
+		
+		
+		
+		
+}
