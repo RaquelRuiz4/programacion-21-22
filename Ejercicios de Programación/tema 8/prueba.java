ffc5607ab4df414f5b369f2e21e19dc2c4a6795c
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-27 19:35:20 +0100

    Add files via upload

---
+++ Ejercicios de Programación/tema 8/prueba.java
@@ -0,0 +1,8 @@
+import funciones.operacionesEnteros.*;
+
+public class prueba{
+	public static void main(String[] args){
+		
+		System.out.println(funciones.operacionesEnteros.quitaPorDetras(23456, 3));
+	}
+}
