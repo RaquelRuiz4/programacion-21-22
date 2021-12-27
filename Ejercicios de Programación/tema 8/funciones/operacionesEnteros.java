3210f17ed413c5c8680b2e8777a4488a0a75db75
RaquelR <raquel.ruiz.alu@iescampanillas.com>
2021-12-27 19:35:05 +0100

    Add files via upload

--- Ejercicios de Programación/tema 8/funciones/operacionesEnteros.java
+++ Ejercicios de Programación/tema 8/funciones/operacionesEnteros.java
@@ -1,3 +1,20 @@
+/* 
+
+7. digitoN: Devuelve el dígito que está en la posición n de un número entero.
+Se empieza contando por el 0 y de izquierda a derecha.
+8. posicionDeDigito: Da la posición de la primera ocurrencia de un dígito
+dentro de un número entero. Si no se encuentra, devuelve -1.
+
+10. quitaPorDelante: Le quita a un número n dígitos por delante (por la
+izquierda).
+11. pegaPorDetras: Añade un dígito a un número por detrás.
+12. pegaPorDelante: Añade un dígito a un número por delante.
+13. trozoDeNumero: Toma como parámetros las posiciones inicial y final
+dentro de un número y devuelve el trozo correspondiente.
+14. juntaNumeros: Pega dos números para formar uno. */
+
+
+
 package funciones;
 
 public class operacionesEnteros{
@@ -50,13 +67,24 @@ public class operacionesEnteros{
 
 	// digitos: Cuenta el número de dígitos de un número entero.
 	public static int digitos(int num){
-		int cont = 0;
-		do{
-			num %= 10;
-			cont++;
-			}
-		while(num > 0);
+		int cont = 0;	
+		while (num > 0){
+			num /= 10;
+			cont++;			
+		}
 		return cont;
 	}
 	
+	// quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha).
+	public static int quitaPorDetras (int num, int quita){
+		for(int i = 0; i < quita; i++){
+			num /= 10;
+		}
+		return num;
+	}
+	
+	// quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda).
+		public static int quitaPorDelante (int num, int quita){
+	
+	}
 }
